cbcff87ca6410d3897df179607a83a0e0689adc5
jarigsepta <jarigsepta8@gmail.com>
2020-05-15 09:51:59 +0700

    add functional, arrow function, class component

---
+++ react-spa/src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// function biasa (component)
+// eslint-disable-next-line
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          [Functional Component] Edit <code>src/App.js</code> and save to
+          reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// arrow function (component)
+// eslint-disable-next-line
+const AppArrow = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          [Arrow Function] Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+// class component
+// eslint-disable-next-line
+class AppClass extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            [Class Component] Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+// export default App;
+// export default AppArrow;
+export default AppClass;
